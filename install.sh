c0567a8311a708db6503eb628e3e22376daaa145
guira8965 <guira8965@gmail.com>
2025-12-13 22:24:41 +0800

    test

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 ### Names for each features.
 SERVER_OPTIMIZATION="gui-optimization"
 MC_SERVER_AUTOMATION="gui-mc-servers"
+CURRENT_USER="/home/$1"
 
 ### Check if script is running with root previleges, if not then prompt for elevation.
 if [ "$EUID" -ne 0 ]; then
@@ -16,7 +17,7 @@ read -p "Press ENTER to continue..."
 
 ### Create directories, just in case they don't exist.
 mkdir -p /usr/local/bin
-mkdir -p $HOME/.config/systemd/user
+mkdir -p $CURRENT_USER/.config/systemd/user
 
 ### Install script and systemd service for server optimization
 echo "Moving $SERVER_OPTIMIZATION.sh to /usr/local/bin/$SERVER_OPTIMIZATION.sh"
@@ -25,10 +26,10 @@ echo "Moving $SERVER_OPTIMIZATION.service to /etc/systemd/system/$SERVER_OPTIMIZ
 cp Features/server-optimization/service.service /etc/systemd/system/$SERVER_OPTIMIZATION.service
 
 ### Install script and systemd service for minecraft server automization
-echo "Moving $MC_SERVER_AUTOMATION.sh to /usr/local/bin/$USER/$MC_SERVER_AUTOMATION.sh"
+echo "Moving $MC_SERVER_AUTOMATION.sh to /usr/local/bin/$CURRENT_USER/$MC_SERVER_AUTOMATION.sh"
 cp Features/mc-servers/script.sh /usr/local/bin/$MC_SERVER_AUTOMATION.sh
-echo "Moving $MC_SERVER_AUTOMATION.service to $HOME/.config/systemd/user/$MC_SERVER_AUTOMATION.service"
-cp Features/mc-servers/service.service $HOME/.config/systemd/user/$MC_SERVER_AUTOMATION.service
+echo "Moving $MC_SERVER_AUTOMATION.service to $CURRENT_USER/.config/systemd/user/$MC_SERVER_AUTOMATION.service"
+cp Features/mc-servers/service.service $CURRENT_USER/.config/systemd/user/$MC_SERVER_AUTOMATION.service
 
 ### Reload Systemd
 echo "Reloading systemd."
