cd15ce93af6d229a9400eac164da7dd256cd828b
guira <39302280+guira8965@users.noreply.github.com>
2025-12-24 14:53:15 +0800

    Migrated to Proxmox

--- Features/server-optimization/script.sh
+++ Features/server-optimization/script.sh
@@ -12,4 +12,16 @@ echo "BD-Prochot disabled."
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo sysctl -w net.ipv6.conf.all.forwarding=1
 
+### Proxmox Logs
+echo "Creating directories for pveproxy pvedaemon pve-cluster"
+mkdir -p /var/log/pveproxy /var/log/pvedaemon /var/log/pve-cluster
+
+echo "Changing permission for pveproxy"
+mkdir -p /var/log/pveproxy
+chown -R www-data:www-data /var/log/pveproxy
+chmod 755 /var/log/pveproxy
+
+### iptables for NAT
+/sbin/iptables -t nat -A POSTROUTING -s 192.168.2.0/24 -o vmbr0 -j MASQUERADE
+
 echo "End of line."
